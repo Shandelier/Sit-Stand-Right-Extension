e2f400e6912e4a74cb9387d6099701fc36ab3fe6
Carlos <26363920+Shandelier@users.noreply.github.com>
2021-10-18 13:57:23 +0200

    standard js

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
-let state = false;
+let state = false
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    if (!state) {
-        chrome.tabs.insertCSS(null, { file: "css/blur.css" });
-        state = !state;
-        return;
-    }
-    chrome.tabs.insertCSS(null, { file: "css/normal.css" });
-    state = !state;
-});
+  if (!state) {
+    chrome.tabs.insertCSS(null, { file: 'css/blur.css' })
+    state = !state
+    return
+  }
+  chrome.tabs.insertCSS(null, { file: 'css/normal.css' })
+  state = !state
+})
