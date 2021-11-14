8bb375c388e746b723ce15eb0917d16ee94e4a70
Carlos <26363920+Shandelier@users.noreply.github.com>
2021-11-14 20:30:10 +0100

    options page experiment

--- src/background.js
+++ src/background.js
@@ -1,129 +1,90 @@
-import 'babel-polyfill';
-import * as tf from '@tensorflow/tfjs';
-import { IMAGENET_CLASSES } from './imagenet_classes';
+const debug = false
 
-const MOBILENET_MODEL_PATH = 'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
-const IMAGE_SIZE = 224;
-const TOPK_PREDICTIONS = 10;
-
-class BackgroundProcessing {
-
+class ActiveTabTracker {
   constructor() {
-    this.imageRequests = {};
-    this.addListeners();
-    this.loadModel();
+    this._activeTab = {}
+    this._tracker = []
   }
 
-  addListeners() {
-    chrome.webRequest.onCompleted.addListener(req => {
-      if (req && req.tabId > 0) {
-        this.imageRequests[req.url] = this.imageRequests[req.url] || req;
-        this.analyzeImage(req.url);
-      }
-    }, { urls: ["<all_urls>"], types: ["image", "object"] });
+  static effects = {
+    blur: 'blur',
+    grey: 'grey'
   }
 
-  async loadModel() {
-    console.log('Loading model...');
-    const startTime = performance.now();
-    this.model = await tf.loadModel(MOBILENET_MODEL_PATH);
-    this.model.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
-
-    const totalTime = Math.floor(performance.now() - startTime);
-    console.log(`Model loaded and initialized in ${totalTime}ms...`);
+  setNewActiveTab(tab) {
+    this._activeTab = tab
+    this.addTabToTracker(tab)
   }
 
-  async loadImage(src) {
-    return new Promise(resolve => {
-      var img = document.createElement('img');
-      img.crossOrigin = "anonymous";
-      img.onerror = function (e) {
-        resolve(null);
-      };
-      img.onload = function (e) {
-        if ((img.height && img.height > 128) || (img.width && img.width > 128)) {
-          // Set image size for tf!
-          img.width = IMAGE_SIZE;
-          img.height = IMAGE_SIZE;
-          resolve(img);
-        }
-        // Let's skip all tiny images
-        resolve(null);
-      }
-      img.src = src;
-    });
-  }
-
-  async getTopKClasses(logits, topK) {
-    const values = await logits.data();
-    const valuesAndIndices = [];
-    for (let i = 0; i < values.length; i++) {
-      valuesAndIndices.push({ value: values[i], index: i });
+  addTabToTracker(tab) {
+    if (this.getTabIndex(tab) === -1) {
+      tab.blur = false
+      tab.grey = false
+      this._tracker.push(tab)
     }
-    valuesAndIndices.sort((a, b) => {
-      return b.value - a.value;
-    });
-    const topkValues = new Float32Array(topK);
-    const topkIndices = new Int32Array(topK);
-    for (let i = 0; i < topK; i++) {
-      topkValues[i] = valuesAndIndices[i].value;
-      topkIndices[i] = valuesAndIndices[i].index;
-    }
-
-    const topClassesAndProbs = [];
-    for (let i = 0; i < topkIndices.length; i++) {
-      topClassesAndProbs.push({
-        className: IMAGENET_CLASSES[topkIndices[i]],
-        probability: topkValues[i]
-      })
-    }
-    return topClassesAndProbs;
   }
 
-
-  async predict(imgElement) {
-    console.log('Predicting...');
-    const startTime = performance.now();
-    const logits = tf.tidy(() => {
-      const img = tf.fromPixels(imgElement).toFloat();
-      const offset = tf.scalar(127.5);
-      const normalized = img.sub(offset).div(offset);
-      const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
-      return this.model.predict(batched);
-    });
-
-    // Convert logits to probabilities and class names.
-    const predictions = await this.getTopKClasses(logits, TOPK_PREDICTIONS);
-    const totalTime = Math.floor(performance.now() - startTime);
-    console.log(`Prediction done in ${totalTime}ms:`, predictions);
-    return predictions;
+  getTabIndex(tab) {
+    var windowId = tab.windowId
+    var tabId = tab.tabId
+    var index = this._tracker.findIndex(t => {
+      return t.tabId === tabId && t.windowId === windowId
+    })
+    return index
   }
 
-  async analyzeImage(src) {
+  getTracker() {
+    return this._tracker
+  }
 
-    if (!this.model) {
-      console.log('Model not loaded yet, delaying...');
-      setTimeout(() => { this.analyzeImage(src) }, 5000);
-      return;
+  getTabStatus(tab) {
+    var index = this.getTabIndex(tab)
+    if (this.debug) {
+      console.log('index', index)
+      console.log('tab under index', this._tracker[index])
     }
+    return this._tracker[index].blur ? this._tracker[index].blur : false
+  }
 
-    var meta = this.imageRequests[src];
-    if (meta && meta.tabId) {
-      if (!meta.predictions) {
-        const img = await this.loadImage(src);
-        if (img) {
-          meta.predictions = await this.predict(img);
-        }
-      }
-
-      if (meta.predictions) {
-        chrome.tabs.sendMessage(meta.tabId, {
-          action: 'IMAGE_PROCESSED',
-          payload: meta,
-        });
-      }
+  setTabStatus(index, status) {
+    if (this.debug) {
+      console.log('settabstatus')
+      console.log('tab index', index)
     }
+    this._tracker[index].blur = status
   }
 }
+// Hooks for tab change event and managing blur effect
+{
+  let isSlouching = false
+  let currentSlouchingEffect = 'blur'
+  let tabTracker = new ActiveTabTracker()
 
-var bg = new BackgroundProcessing();
+  function updateEffect(isSlouching, tab) {
+    var tabEffectState = tabTracker.getTabStatus(tab)
+    var tabIndex = tabTracker.getTabIndex(tab)
+    if (isSlouching !== tabEffectState) {
+      if (isSlouching) {
+        chrome.tabs.insertCSS(null, { file: 'src/css/blur.css' })
+        tabTracker.setTabStatus(tabIndex, true)
+        return
+      }
+      chrome.tabs.insertCSS(null, { file: 'src/css/normal.css' })
+      tabTracker.setTabStatus(tabIndex, false)
+      return
+    }
+  }
+
+  chrome.browserAction.onClicked.addListener(function (activeInfo) {
+    var tab = { tabId: activeInfo.id, windowId: activeInfo.windowId }
+    // isSlouching = !isSlouching
+    chrome.runtime.openOptionsPage()
+    updateEffect(isSlouching, tab)
+  })
+
+  chrome.tabs.onActivated.addListener(function (tab) {
+    tabTracker.setNewActiveTab(tab)
+    updateEffect(isSlouching, tab)
+  })
+
+}
---
+++ src/options/options.js
@@ -0,0 +1,6 @@
+
+import '@mediapipe/pose'
+import * as posedetection from '@tensorflow-models/pose-detection'
+import * as tf from '@vladmandic/tfjs'
+
+console.log(posedetection, tf)
